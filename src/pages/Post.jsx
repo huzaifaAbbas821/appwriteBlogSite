1a3522a157a89646ce03a35e6c281c87bb679ac6
huzaifaAbbas821 <huzaifaabbas8121@gmail.com>
2024-07-22 23:56:38 +0500

    fixing some issues

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -4,6 +4,10 @@ import appwriteService from "../appwrite/config";
 import { Button, Container } from "../components";
 import parse from "html-react-parser";
 import { useSelector } from "react-redux";
+import { MdOutlineModeEdit } from "react-icons/md";
+import { MdDeleteOutline } from "react-icons/md";
+
+
 
 export default function Post() {
   const [post, setPost] = useState(null);
@@ -36,26 +40,28 @@ export default function Post() {
     <div className="max-w-7xl mx-auto my-6 px-4 sm:px-6 lg:px-8 ">
       <div className="max-w-[100vw] md:max-w-3xl mx-auto px-4 py-4 bg-slate-100 relative">
         {isAuthor && (
-          <div className="absolute right-6 top-6">
+          <div className="absolute right-2 top-1 flex flex-row justify-center items-center">
             <Link to={`/edit-post/${post.$id}`}>
-              <Button bgColor="bg-green-500" className="mr-3">
-                Edit
+              <Button bgColor="bg-green-500" className="mr-3 flex flex-row gap-2">
+                <p className="hidden md:inline-block"> Edit</p>
+                <MdOutlineModeEdit/>
               </Button>
             </Link>
-            <Button bgColor="bg-red-500" onClick={deletePost}>
-              Delete
+            <Button bgColor="bg-red-500 flex gap-2 justify-center items-center" onClick={deletePost}>
+               <p className="hidden md:inline-block">Delete</p>
+              <MdDeleteOutline />
             </Button>
           </div>
         )}
         <div className="py-8 ">
-          <h1 className="text-3xl font-bold mb-2">{post.title}</h1>
+          <h1 className="text-3xl font-bold mb-2 break-words">{post.title}</h1>
         </div>
         <img
           src={appwriteService.getFilePreview(post.featuredImage)}
           alt="Featured image"
           className="w-full h-auto mb-8"
         />
-        <div className="prose prose-sm sm:prose lg:prose-lg xl:prose-xl mx-auto">
+        <div className="prose prose-sm sm:prose lg:prose-lg xl:prose-xl mx-auto break-words">
           {parse(post.content)}
         </div>
       </div>
